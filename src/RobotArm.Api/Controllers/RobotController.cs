f2590a4efd60d242e50a9fc0710cedd21868c189
Vent, Merijn <PS265830@summacollege.nl>
2026-09-15 11:28:53 +0200

    Controllers en confiuguratie

---
+++ src/RobotArm.Api/Controllers/RobotController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RobotArm.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RobotController : ControllerBase
+    {
+        [HttpGet("status")]
+        public IActionResult GetStatus()
+        {
+            return Ok(new
+            {
+                status = "online",
+                message = "RobotArm API werkt"
+            });
+        }
+    }
+}
